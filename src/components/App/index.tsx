53aaf6f96b8ff56ef2a6d388c32d965ad972ca5f
Shuntaro Miwa <shuntaro.miwa@MacBook-Pro-2.local>
2022-11-15 19:52:16 +0900

    chore: @fourdigit/sanitize-4d.css

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '~/assets/react.svg'
 import s from './index.module.scss'
+import '@fourdigit/sanitize-4d.css';
 import clsx from 'clsx'
 
 function App() {
